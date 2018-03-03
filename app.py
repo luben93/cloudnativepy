10a8f0e11e6469f9768d79f3945036faaf8573a6
lucas persson <luben93@gmail.com>
2018-03-03 02:04:24 +0100

    at page 66

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import abort
 
 import json
 import sqlite3
-
+import strftime
 
 app = Flask(__name__)
 
@@ -70,6 +70,19 @@ def update_user(user_id):
 
 
 
+@app.route('/api/v2/tweets', methods=['GET'])
+def get_tweets():
+    return list_tweets()
+
+@app.route('/api/v2/tweets', methods=['POST'])
+def add_tweets():
+    user_tweet = {}
+    if not request.json or not 'username' in request.json or not 'body' in request.json:
+        abort(400)
+    user_tweet['username'] = request.json['username']
+    user_tweet['body'] = request.json['body']
+    user_tweet['created_at'] = strftime("%Y-%m-%dT%H:%M:%SZ", gmtime())
+    return jsonify({'status':add_tweet(user_tweet)}), 200
 
 
 
@@ -81,11 +94,35 @@ def update_user(user_id):
 
 
 
+def add_tweet(new_tweet):
+    conn = sqlite3.connect("mydb.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from users where username=? ", (new_tweet['username'],))
+    data.fetchall()
+    if len(data) == 0:
+        abort(404)
+    else:
+        cursor.execute("INSERT into tweets (username,body,tweet_time) values(?,?,?)",(new_tweet['username'],new_tweet['body'],new_tweet['created_at']))
+        conn.commit()
+    return "success"
 
-
-
-
-
+def list_tweets():
+    conn = sqlite3.connect('mydb.db')
+    api_list = []
+    cursor = conn.execute("SELECT username, body, tweet_time, id from tweets")
+    data = cursor.fetchall()
+    if len(data) != 0:
+        for row in cursor:
+            tweets = {}
+            tweets['Tweet by'] = row[0]
+            tweets['Body'] = row[1]
+            tweets['Timestamp'] = row[2]
+            tweets['id'] = row[3]
+            api_list.append(tweets)
+    else:
+        return api_list
+    conn.close()
+    return jsonify({'tweets_list':api_list})
 
 
 def upd_user(user):
